6b0030ecb901933180b6121d17a193c7cb9a970a
khunrumejjj <khunrumejjj@gmail.com>
2017-11-19 14:50:57 +0700

    Testing upload to github

---
+++ src/oop_lab/s359211110086/rmutsv/com/HelloWold.java
@@ -0,0 +1,4 @@
+package oop_lab.s359211110086.rmutsv.com;
+
+public class HelloWold {
+}
---
+++ src/oop_lab/s359211110086/rmutsv/com/HelloWorld.java
@@ -0,0 +1,15 @@
+package oop_lab.s359211110086.rmutsv.com;
+
+public class HelloWorld {
+
+ // create main method
+ public static void main(String[] args) {
+
+     System.out.println("Hello World, I'm khun");
+
+
+
+ }  //main
+
+
+}//class
