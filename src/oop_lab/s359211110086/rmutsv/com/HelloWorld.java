b137449b5227f6e34ba6460ee0fc40ee15d3e827
khunrumejjj <khunrumejjj@gmail.com>
2017-11-19 15:18:49 +0700

    Add information

--- src/oop_lab/s359211110086/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s359211110086/rmutsv/com/HelloWorld.java
@@ -6,6 +6,9 @@ public class HelloWorld {
  public static void main(String[] args) {
 
      System.out.println("Hello World, I'm khun");
+     System.out.println("Rungaroon Rattanaphun");
+     System.out.println("Information System");
+     System.out.println("MT:RMUTSV");
 
 
 
