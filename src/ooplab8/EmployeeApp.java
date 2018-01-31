db5113caf7b67af02562d77fa0f1dae2915db23e
khunrumejjj <khunrumejjj@gmail.com>
2018-01-31 10:29:25 +0700

    Employee extends Person

--- src/ooplab8/EmployeeApp.java
+++ src/ooplab8/EmployeeApp.java
@@ -2,7 +2,7 @@ package ooplab8;
 
 public class EmployeeApp {
     public static void main(String[] args) {
-        Employee employee = new Employee("Boy Saiyai","Male","27/12/2538",22,"sale",30000);
+        Employee employee = new Employee("Boy Saiyai","Male","27/12/2538",22,"Marketing",30000);
         System.out.println(employee.toString());
     }//main
 }//class
